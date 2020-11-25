7615838e08e0a2e4e7c632f0689d9c0d1a90d9a2
trisolaris3body <19512282+trisolaris3body@users.noreply.github.com>
2020-11-25 18:31:50 -0500

    Refactored addFlashcard cloud function into 2 methods for readability

--- functions/index.js
+++ functions/index.js
@@ -17,61 +17,67 @@ app.use((req, res, next) => {
 });
 
 app.post('/addFlashcard', async (req, res) => {
-  const data = req.body;
-  //const uid = auth.currentUser.uid;
-  const uid = req.body.uid;
+  const userUid = req.body.uid;
 
-  // get song from Musixmatch api
-  let word = data.front;
-  let lyric;
-  let page_size = 3;
-  let s_track_rating = 'desc';
-  let language = 'fr';
+  const musixmatchParams = {
+    front: req.body.front,
+    q_lyrics: req.body.front,
+    language: req.body.language ? req.body.language : 'fr',
+    s_track_rating: 'desc',
+    page_size: 3,
+    format: 'json',
+    song: 'placeholder',
+  };
 
+  await getSongFromMusixmatch(musixmatchParams);
+  console.log('outside await getSong...song=', musixmatchParams.song);
+
+  //TODO: get spotify uri of song
+
+  const cardInfo = {
+    front: req.body.front,
+    back: req.body.back,
+    song: musixmatchParams.song,
+    source: req.body.source,
+    spotifySongUri: '',
+  };
+  await saveFlashcard(userUid, cardInfo);
+
+  res.status(201).send();
+});
+exports.webAPI = functions.https.onRequest(app);
+
+/* Helper functions */
+async function getSongFromMusixmatch(musixmatchParams) {
   //TODO: use params obj and pass to axios
   let url =
     'https://api.musixmatch.com/ws/1.1/track.search?format=json&q_lyrics=' +
-    word +
+    musixmatchParams.q_lyrics +
     '&f_lyrics_language=fr&s_track_rating=desc&page_size=1&page=1&apikey=' +
     config.MUSIXMATCH_API_KEY;
-  // language (or no language), or require users to set language, or figure it out in background
-  var song = 'placeholder';
 
   await axios
     .get(url)
     .then((response) => {
-      console.log(response.data.message.body.track_list[0].track);
-      song = response.data.message.body.track_list[0].track.track_name;
-      console.log('SONG TITLE IS: ', song);
+      musixmatchParams.song =
+        response.data.message.body.track_list[0].track.track_name;
+      console.log('SONG TITLE IS: ', musixmatchParams.song);
     })
     .catch((error) => console.log('error in axios.get, ', error));
+}
 
-  //TODO: spotify uri of song
-
-  // Save flashcard
+async function saveFlashcard(user_uid, cardInfo) {
   await admin
     .firestore()
     .collection('users')
-    .doc(uid)
+    .doc(user_uid)
     .collection('flashcards')
     .add({
-      front: data.front,
-      back: data.back,
-      song: song,
-      source: data.source,
+      front: cardInfo.front,
+      back: cardInfo.back,
+      song: cardInfo.song,
+      source: cardInfo.source,
+      spotifySongUri: cardInfo.spotifySongUri,
     });
-  console.log(song);
-
-  res.status(201).send();
-});
-// sets up an endpoint
-exports.webAPI = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+}
 // https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=bonjour&f_music_genre_id=14&f_lyrics_language=fr&s_track_rating=desc&page_size=5&page=1&apikey=ee5932e58d6a546d292e753097d44d47
